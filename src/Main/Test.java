259ee546d0cd7e78893e0deed0f0b1d716414bc9
Amisha0971 <amishaninawe971@gmail.com>
2025-07-25 15:27:54 +0530

    first commit

---
+++ src/Main/Test.java
@@ -0,0 +1,17 @@
+package Main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import Model.Question;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		 Question q=(Question)context.getBean("q");  
+		  q.displayInfo();  
+
+	}
+
+}
